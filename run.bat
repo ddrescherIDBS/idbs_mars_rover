cd5d2da600ad1cda4ba726c5b57a6e0e42632409
ddrescherIDBS <dylan.drescher@idbs.com>
2025-06-05 12:37:59 +0100

    Update batch file

--- run.bat
+++ run.bat
@@ -1,4 +1,9 @@
+set "DIR=idbs_mars_rover"
+
+if exist "%DIR%\" (
+    rmdir /s /q "%DIR%"
+)
+
 git clone https://github.com/ddrescherIDBS/idbs_mars_rover.git
 cd idbs_mars_rover
-mvn install
-java -cp "target/idbs_mars_rover-1.0-SNAPSHOT.jar" "com.codemanship.marsrover.Program"
+mvn install & java -cp "target/idbs_mars_rover-1.0-SNAPSHOT.jar" "com.codemanship.marsrover.Program"
