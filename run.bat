8a3f0688c6c3b3bf1d464c58b8eb35123939b455
ddrescherIDBS <dylan.drescher@idbs.com>
2025-06-05 12:23:34 +0100

    Updates to run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 git clone https://github.com/ddrescherIDBS/idbs_mars_rover.git
 cd idbs_mars_rover
 mvn install
-java -jar "target/idbs_mars_rover-1.0-SNAPSHOT.jar"
+java -cp "target/idbs_mars_rover-1.0-SNAPSHOT.jar" "com.codemanship.marsrover.Program"
